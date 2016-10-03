00341c2930fe391040ba7ae58987bf10b4d82848
santiago_delgado <arcana18_18@hotmail.com>
2016-10-03 11:12:00 -0500

    Agrega configuración para CI Travis Se agregó el archivo .travis.yml, requerimientos.txt para poder realizar CI en travis.ci.org

--- figuras.py
+++ figuras.py
@@ -1,39 +1,40 @@
 from math import pi
+
+
 class Figuras:
-	def cuadrado(self,lado):
 
-		try:
-			lado=int(lado)
-			return lado * lado
-		except Exception, e:
-			return 'dato incorrecto'
+    def cuadrado(self, lado):
 
-	def rectangulo(self,b,h):
-		try:
-			b=int(b)
-			h=int(h)
-			return (b*h)
-		except Exception, e:
-			return 'dato incorrecto'
+        try:
+            lado = int(lado)
+            return lado * lado
+        except Exception, e:
+            return 'dato incorrecto'
 
-	def triangulo(self,b,h):
-		try:
-			b=int(b)
-			h=int(h)
-			return (b*h)/2
-		except Exception, e:
-			return 'dato incorrecto'
+    def rectangulo(self, b, h):
+        try:
+            b = int(b)
+            h = int(h)
+            return (b * h)
+        except Exception, e:
+            return 'dato incorrecto'
 
-	def circulo(self,radio):
-		try:
-			radio=int(radio)
-			pi=3.1416
+    def triangulo(self, b, h):
+        try:
+            b = int(b)
+            h = int(h)
+            return (b * h) / 2
+        except Exception, e:
+            return 'dato incorrecto'
 
-			radioC=radio*radio
-			#result= math.pi*radioC
-			result=pi*radioC
-			return result
-		except Exception, e:
-			return 'dato incorrecto'
+    def circulo(self, radio):
+        try:
+            radio = int(radio)
+            pi = 3.1416
 
-			
+            radioC = radio * radio
+            #result= math.pi*radioC
+            result = pi * radioC
+            return result
+        except Exception, e:
+            return 'dato incorrecto'
